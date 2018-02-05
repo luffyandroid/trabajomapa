e181236d932d04a9e46704972574e9a94a9c6a8c
luffyandroid <luisjesus.delgado89@gmail.com>
2018-02-05 02:25:44 +0100

    hecho en casa 4 de febrero 2018

--- app/src/main/java/com/example/didact/u3_ejerciciospinner2pizzeria/MainActivity.java
+++ app/src/main/java/com/example/didact/u3_ejerciciospinner2pizzeria/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -18,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup rgTamano;
     CheckBox cbHamburguesa, cbJamon, cbPepperonni, cbPimiento, cbCebolla, cbPina,
             cbPollo, cbAtun, cbAceituna;
-    TextView tvMasaResultado;
+    TextView tvMasaResultado, tvIngredienteResulado, tvTamanoResultado;
+    Button btnIngrediente, btnTamano;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
         cbPollo = (CheckBox)findViewById(R.id.cb_pollo);
         cbAtun = (CheckBox)findViewById(R.id.cb_atun);
         tvMasaResultado = (TextView)findViewById(R.id.tv_masaresultado);
+        tvIngredienteResulado = (TextView)findViewById(R.id.tv_ingrediente);
+        tvTamanoResultado = (TextView)findViewById(R.id.tv_tamano);
+        btnIngrediente = (Button)findViewById(R.id.btn_ingrediente);
+        btnTamano = (Button)findViewById(R.id.btn_tamano);
 
         String[] masa={"","Fina","Mediana","Gorda"};
         ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,
@@ -49,19 +56,134 @@ public class MainActivity extends AppCompatActivity {
         String masaSeleccionada = spMasa.getSelectedItem().toString();
 
         if(masaSeleccionada.equals("")){
-            Toast.makeText(this,"Debes seleccionar un tipo de masa",
+            Toast.makeText(this, "Debes seleccionar un tipo de masa",
                     Toast.LENGTH_LONG).show();
+            btnIngrediente.setEnabled(false);
+            btnTamano.setEnabled(false);
+            tvMasaResultado.setText("Tipo masa: ");
+            tvIngredienteResulado.setText("Ingredientes: ");
+            tvTamanoResultado.setText("Tamaño: ");
         }else{
-
+            tvMasaResultado.setText("Tipo masa: "+masaSeleccionada);
+            btnIngrediente.setEnabled(true);
         }
 
     }
 
     public void clickBtnIngrediente(View view){
 
+        String pedidoingredientes = "";
+
+        if(cbAceituna.isChecked()){
+            pedidoingredientes += " aceituna";
+        }
+
+        if(cbAtun.isChecked()){
+            pedidoingredientes += " atun";
+        }
+
+        if(cbJamon.isChecked()){
+            pedidoingredientes += " jamon";
+        }
+
+        if(cbHamburguesa.isChecked()){
+            pedidoingredientes += " hamburguesa";
+        }
+
+        if(cbPepperonni.isChecked()){
+            pedidoingredientes += " pepperonni";
+        }
+
+        if(cbPimiento.isChecked()){
+            pedidoingredientes += " pimiento";
+        }
+
+        if(cbPina.isChecked()){
+            pedidoingredientes += " piña";
+        }
+
+        if(cbPollo.isChecked()){
+            pedidoingredientes += " pollo";
+        }
+
+        if(cbCebolla.isChecked()){
+            pedidoingredientes += " cebolla";
+        }
+
+        if(pedidoingredientes.equals("")){
+            Toast.makeText(this, "Debes seleccionar al menos un ingrediente",
+                    Toast.LENGTH_LONG).show();
+            btnTamano.setEnabled(false);
+            tvIngredienteResulado.setText("Ingredientes ");
+            tvTamanoResultado.setText("Tamaño: ");
+        }else{
+            tvIngredienteResulado.setText("Ingredientes: "+pedidoingredientes);
+            btnTamano.setEnabled(true);
+        }
+
+
     }
 
     public void clickBtnTamano(View view){
+
+        int idRadio = rgTamano.getCheckedRadioButtonId();
+
+        if (idRadio == -1){
+            Toast.makeText(this, "Debes seleccionar un tamano",
+                    Toast.LENGTH_SHORT).show();
+            tvTamanoResultado.setText("Tamaño: ");
+        }else {
+
+            RadioButton radioButtonSeleccionado = (RadioButton) findViewById(idRadio);
+
+            String tamanoRadioSeleccionado = radioButtonSeleccionado.getText().toString();
+
+            String masaSeleccionada = spMasa.getSelectedItem().toString();
+
+            String pedidoingredientes = "";
+
+            if(cbAceituna.isChecked()){
+                pedidoingredientes += " aceituna";
+            }
+
+            if(cbAtun.isChecked()){
+                pedidoingredientes += " atun";
+            }
+
+            if(cbJamon.isChecked()){
+                pedidoingredientes += " jamon";
+            }
+
+            if(cbHamburguesa.isChecked()){
+                pedidoingredientes += " hamburguesa";
+            }
+
+            if(cbPepperonni.isChecked()){
+                pedidoingredientes += " pepperonni";
+            }
+
+            if(cbPimiento.isChecked()){
+                pedidoingredientes += " pimiento";
+            }
+
+            if(cbPina.isChecked()){
+                pedidoingredientes += " piña";
+            }
+
+            if(cbPollo.isChecked()){
+                pedidoingredientes += " pollo";
+            }
+
+            if(cbCebolla.isChecked()){
+                pedidoingredientes += " cebolla";
+            }
+
+            tvTamanoResultado.setText("Tamaño: "+tamanoRadioSeleccionado);
+
+            Toast.makeText(this,"Usted ha pedido una pizza de masa "+masaSeleccionada+" con "+pedidoingredientes
+            +" y tamano "+tamanoRadioSeleccionado,Toast.LENGTH_LONG).show();
+        }
+
 
     }
 
