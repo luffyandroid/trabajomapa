d7de1a482a787d9d476f77ecaf9e4d791a7b0181
fulgenl <fulgencio@didact.es>
2018-11-15 09:54:50 +0100

    Implementado PayPal y Activitys implicados

---
+++ app/src/main/java/com/example/fl/trabajomapa/Config/Config.java
@@ -0,0 +1,7 @@
+package com.example.fl.trabajomapa.Config;
+
+public class Config {
+
+    //CUENTA TOKEN PAYPAL
+    public static final String PAYPAL_CLIENT_ID ="AcNPl3gbbcj5xc8sP47ZJYtmTj0IPG19F762YwrQDt-VMriSiAqn0wZJNgiNdsDMAwHuZ02IQA60IrY4";
+}
